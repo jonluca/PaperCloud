bffd928266599962db76fe56be8c8ec7b31d3335
Myrl Marmarelis <myrl@Myrls-MacBook-Pro.local>
2017-04-23 21:28:06 -0700

    search by author and keyword

--- php/get_IEEE_list.php
+++ php/get_IEEE_list.php
@@ -2,21 +2,25 @@
 
 if (defined('STDIN')) {
     $search = $argv[1];
-} else { 
+    if (isset($argv[2])) {
+        $type = $argv[2];
+    }
+} else {
     $search = $_GET["search"];
+    if (isset($_GET["type"])) {
+        $type = $_GET["type"]; // this can be "author" or "keyword"
+    }
 }
 
-function performQuery($search) {
-
-    $url = "https://ieeexplore.ieee.org/gateway/ipsSearch.jsp?au=$search";
+function performQuery($search, $type) {
+    $cmd = ($type == "keyword")? "idxterms" : "au";
+    $url = "https://ieeexplore.ieee.org/gateway/ipsSearch.jsp?$cmd=$search";
     $xml_result = file_get_contents($url);
     $xml = simplexml_load_string($xml_result, 'SimpleXMLElement', LIBXML_NOCDATA);
-	$json = json_encode($xml);
+	   $json = json_encode($xml);
     return $json;
 }
 
-echo (performQuery($search));
-
-// not sure if we should parse in PHP but it's easy with: new SimpleXMLElement("..");
+echo (performQuery($search, $type));
 
 ?>
